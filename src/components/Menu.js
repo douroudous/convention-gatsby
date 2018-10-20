b8fc34ef5b354c25445b3d2c53ad2d5e763ddf0a
Dino Dourountoudakis <CDOUROUNTOUDAKIS@GMAIL.COM>
2018-10-20 14:28:41 -0400

    first commit

---
+++ src/components/Menu.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
+
+const Menu = (props) => (
+    <nav id="menu">
+        <div className="inner">
+            <ul className="links">
+                <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/landing">Events</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/landing">Landing</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/generic">Generic</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/elements">Elements</Link></li>
+            </ul>
+        </div>
+        <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>
+    </nav>
+)
+
+Menu.propTypes = {
+    onToggleMenu: PropTypes.func
+}
+
+export default Menu
