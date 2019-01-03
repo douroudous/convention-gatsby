912d27b4152c56d6f7edf33a7ccf3edf44f665bc
Dino Dourountoudakis <CDOUROUNTOUDAKIS@GMAIL.COM>
2019-01-02 22:06:07 -0500

    blurbs

--- src/pages/entertainment.js
+++ src/pages/entertainment.js
@@ -40,7 +40,7 @@ const Entertainment = (props) => (
                             <header className="major">
                                 <h3>Michalis Tzouganakis</h3>
                             </header>
-                            <p>Renowned laouto player Michalis Tzouganakis has brought about important changes to the Cretan traditional song, not only with his interpretation of songs but also with the contemporary sounds he added, without distorting the traditional element. <a href="https://www.last.fm/music/Michalis+Tzouganakis/+wiki" rel="noopener noreferrer" target="_blank">(source)</a></p>
+                            <p>World-renowned laouto player Michalis Tzouganakis and his band.</p>
                             <ul className="actions">
                                 <li><a href="https://www.facebook.com/MichalisTzouganakis/" rel="noopener noreferrer" target="_blank" className="button">Facebook</a></li>
                                 <li><a href="https://www.youtube.com/channel/UCVvAUAmi5F4aojmEZS0P6Ew" rel="noopener noreferrer" target="_blank" className="button">YouTube</a></li>
@@ -75,7 +75,7 @@ const Entertainment = (props) => (
                             <header className="major">
                                 <h3>Viglatores Chania</h3>
                             </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+                            <p>Founded in May 2009, "Viglatores" is a traditional dance association with over 200 active registered members. Their basic purpose is the conveying of dance tradition in its purest form, teaching 22 Cretan dances as well as dances from the rest of Greece.</p>
                             <ul className="actions">
                                 <li><a href="https://www.facebook.com/viglatores09/" rel="noopener noreferrer" target="_blank" className="button">Facebook</a></li>
                                 <li><a href="https://www.youtube.com/user/viglatores09/" rel="noopener noreferrer" target="_blank" className="button">YouTube</a></li>
@@ -92,7 +92,8 @@ const Entertainment = (props) => (
                             <header className="major">
                                 <h3>Pancretan Youth of America</h3>
                             </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+                            <p>The PYA is the oldest federation of Greek American youth organizations in the nation, with twenty organized youth chapters through the US.</p>
+                            <p>Is your dance group interested in performing at the convention? Please contact Alex Hatzis at <a href="mailto:alexander_hatzis@yahoo.com">alexander_hatzis@yahoo.com</a>.</p>
                             <ul className="actions">
                                 <li><a href="https://www.facebook.com/groups/14049154750/" rel="noopener noreferrer" target="_blank" className="button">Facebook</a></li>
                             </ul>
