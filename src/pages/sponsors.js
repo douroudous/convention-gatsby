03053b51c5c595ff22f6a19991728261fd06b522
Dino Dourountoudakis <cdourountoudakis@gmail.com>
2019-06-25 08:15:45 -0400

    sponsor start

--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -6,6 +6,18 @@ import GenericBanner from '../components/GenericBanner'
 import sponsorForm from '../assets/images/sponsorship-form.pdf'
 import albumForm from '../assets/images/album-form.pdf'
 
+// A Special Thanks to our Sponsors of the
+// 46th PAA National Convention
+// hosted by
+// Cretan’s Association Minos-Crete, Proodos, & Minos-Crete Youth of Springfield, MA.
+
+// Platinum Sponsors
+// Church & Main Deli-Café Costas Bouzakis & Family Elias Bouzakis & Family Kalamarakis Family Peter Kantos & Family John Kontekakis Marnelakis Family Oak Barrel Wine & Spirits Manny Rovithis White Birch Insurance
+// Gold Sponsors
+// John Bertakis Steve’s Boston Seafood Titan Foods George Trikounakis
+// Silver Sponsors
+// Costas Dourountoudakis & Family Mike Dourountoudakis & Family Anna Giannoulias
+// St. George Cathedral George Tzanidakis
 const Sponsors = (props) => (
     <Layout>
         <Helmet>
@@ -18,6 +30,13 @@ const Sponsors = (props) => (
         />
 
         <div id="main">
+        <section id="one">
+            <div className="inner">
+                <header className="major">
+                    <h2>Music</h2>
+                </header>
+            </div>
+        </section>
             <section className="spotlights">
                 <section>
                     <div className="image">
